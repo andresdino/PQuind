2bbfd8187258b2326a4ceb95163c0a2cdfa79407
andresdino <andresdino9ster@gmail.com>
2024-02-01 11:26:27 -0500

    feature/dino : Creacion constantes para aplicacion

---
+++ src/main/java/com/example/pruebatecnicaquind/Constants/MessageAplication.java
@@ -0,0 +1,8 @@
+package com.example.pruebatecnicaquind.Constants;
+
+public class MessageAplication {
+
+    public static String DELETECLIENTERROR= "No se puede eliminar el cliente porque tiene productos vinculados.";
+    public static String BALANCECANNOTLESS0 = "El saldo de una cuenta de ahorros no puede ser menor a $0.";
+
+}
