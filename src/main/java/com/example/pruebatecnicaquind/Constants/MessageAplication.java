b5fa4045beb22cca873d12b2f34f7f8f6ac432cb
andresdino <andresdino9ster@gmail.com>
2024-02-01 14:56:55 -0500

    feature/dino : Modificacion mensajes genericos

--- src/main/java/com/example/pruebatecnicaquind/Constants/MessageAplication.java
+++ src/main/java/com/example/pruebatecnicaquind/Constants/MessageAplication.java
@@ -4,5 +4,7 @@ public class MessageAplication {
 
     public static String DELETECLIENTERROR= "No se puede eliminar el cliente porque tiene productos vinculados.";
     public static String BALANCECANNOTLESS0 = "El saldo de una cuenta de ahorros no puede ser menor a $0.";
+    public static String ACCOUNTNOTFOUND = "No se encontro la cuenta";
+    public static String ACCOUNTCANNOTCANCELLED  = "No se puede cancelar la cuenta";
 
 }
