ff1191aa096a61d6111c5b6a103ad6d1335ce74a
andresdino <andresdino9ster@gmail.com>
2024-02-01 14:55:12 -0500

    feature/dino : Modificacion nombre clase Enums

--- src/main/java/com/example/pruebatecnicaquind/Config/EstadoCuenta.java
+++
@@ -1,7 +0,0 @@
-package com.example.pruebatecnicaquind.Entity.Cuentas;
-
-public enum EstadoCuenta {
-    ACTIVADA,
-    INACTIVA,
-    CANCELADA
-}
---
+++ src/main/java/com/example/pruebatecnicaquind/Enums/EstadoCuenta.java
@@ -0,0 +1,7 @@
+package com.example.pruebatecnicaquind.Enums;
+
+public enum EstadoCuenta {
+    ACTIVA,
+    INACTIVA,
+    CANCELADA
+}
