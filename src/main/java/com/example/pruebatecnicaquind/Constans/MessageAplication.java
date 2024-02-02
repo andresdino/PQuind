ffefce19e557a65213241db3edd574f479f180dd
andresdino <andresdino9ster@gmail.com>
2024-02-02 00:45:16 -0500

    feature/refactor : Creacion Mensajes genericos

---
+++ src/main/java/com/example/pruebatecnicaquind/Constans/MessageAplication.java
@@ -0,0 +1,15 @@
+package com.example.pruebatecnicaquind.Constans;
+
+public class MessageAplication {
+
+    public static String DELETECLIENTERROR= "No se puede eliminar el cliente porque tiene productos vinculados.";
+    public static String DELETECLIENT = "Se borro la informacion del cliente correctamente";
+    public static String BALANCECANNOTLESS0 = "El saldo de una cuenta de ahorros no puede ser menor a $0.";
+    public static String ACCOUNTNOTFOUND = "No se encontro la cuenta";
+    public static String CLIENTNOTFOUND = "No se encontro el cliente a eliminar";
+    public static String ACCOUNTCANNOTCANCELLED  = "No se puede cancelar la cuenta";
+    public static String ACCOUNTCANCELLED  = "Cuenta Cancelada";
+    public static String CANNOTCREATEDIFFERENTACCOUNT  = "No se puede crear la cuenta";
+    public static String UPDATEACCOUNTSTATUSCORRECTLY = "Se actualizo el estado de la cuenta correctamente";
+    public static String AMOUNTAPPROPRIATIONMUSTBEPOSITIVE = "El monto de consignación debe ser positivo.";
+}
