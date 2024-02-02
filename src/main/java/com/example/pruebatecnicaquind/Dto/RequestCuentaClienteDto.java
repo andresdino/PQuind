127969eaba0d84be49945aa0143f057ec57fbdc8
andresdino <andresdino9ster@gmail.com>
2024-02-02 09:38:39 -0500

    feature/refactor : Update RequestCuentaClienteDto

--- src/main/java/com/example/pruebatecnicaquind/Dto/RequestCuentaClienteDto.java
+++ src/main/java/com/example/pruebatecnicaquind/Dto/RequestCuentaClienteDto.java
@@ -1,13 +1,7 @@
 package com.example.pruebatecnicaquind.Dto;
 
-
-import com.example.pruebatecnicaquind.Entity.Auditoria;
-import com.example.pruebatecnicaquind.Enum.EstadoCuenta;
-import com.example.pruebatecnicaquind.Enum.TipoCuenta;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.math.BigDecimal;
 
 
 @Getter
